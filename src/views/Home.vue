9c82c1d9cd2e17dd8ddbde9cbf57778cb90d2ad9
Marcus Blättermann <kontakt@essenmitsosse.de>
2020-05-28 22:34:14 +0200

    Change indention

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    
-  </div>
+	<div class="home">
+
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {}
+	name: 'Home',
+	components: {}
 }
 </script>

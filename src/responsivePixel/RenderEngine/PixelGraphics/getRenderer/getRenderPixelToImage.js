1a52e988ceaf9b7af5b21854dd0392004fba643b
Marcus Blättermann <kontakt@essenmitsosse.de>
2020-06-01 00:57:15 +0200

    Fix eslint errors

--- src/responsivePixel/RenderEngine/PixelGraphics/getRenderer/getRenderPixelToImage.js
+++ src/responsivePixel/RenderEngine/PixelGraphics/getRenderer/getRenderPixelToImage.js
@@ -1,26 +1,35 @@
 export function getRenderPixelToImage(backgroundColor) {
 	return function renderPixelToImage(pixelW, pixelH, pixelArray, imageData) {
-		let pW = pixelW; let w4 = pW * 4; const wFull = w4; let pH; const pHSave = pixelH; const fullSave = w4 * pHSave; let full; let c; let i; let row; const pA = pixelArray; const defaultRed = backgroundColor && backgroundColor[0]; const defaultGreen = backgroundColor && backgroundColor[1]; const
-			defaultBlue = backgroundColor && backgroundColor[2];
-		while (pW--) {
+		let sizeXPixel = pixelW;
+		let w4 = sizeXPixel * 4;
+		const wFull = w4;
+		const pHSave = pixelH;
+		const fullSave = w4 * pHSave;
+		const pA = pixelArray;
+		while (sizeXPixel) {
+			sizeXPixel -= 1;
 			w4 -= 4;
-			pH = pHSave;
-			full = fullSave;
-			row = pA[pW];
-			while (pH--) {
-				if ((c = row[pH].s.pop())) {
-					c = c.c;
-					imageData[i = w4 + (full -= wFull)] = c[0];
-					imageData[i += 1] = c[1];
-					imageData[i += 1] = c[2];
-					imageData[i += 1] = 255;
-				} else if (backgroundColor) {
-					imageData[i = w4 + (full -= wFull)] = defaultRed;
-					imageData[i += 1] = defaultGreen;
-					imageData[i += 1] = defaultBlue;
-					imageData[i += 1] = 255;
+			let sizeYPixel = pHSave;
+			let full = fullSave;
+			const row = pA[sizeXPixel];
+			while (sizeYPixel) {
+				sizeYPixel -= 1;
+				const c = row[sizeYPixel].s.pop();
+				const i = w4 + (full -= wFull);
+				const color = c !== undefined
+					? c.c
+					: backgroundColor;
+
+				/* eslint-disable no-param-reassign */
+				if (color) {
+					[
+						imageData[i],
+						imageData[i + 1],
+						imageData[i + 2],
+					] = color;
+					imageData[i + 3] = 255;
 				} else {
-					imageData[i = w4 + (full -= wFull) + 3] = 0;
+					imageData[i + 3] = 0;
 				}
 			}
 		}

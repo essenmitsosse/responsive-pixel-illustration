577f1ebc266c5e62ada54c6f8cf66fefe3cf3534
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-13 01:01:38 +0100

    Turn of some eslint for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 			files: ["*.ts", "*.vue"],
 			extends: [
 				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				// "plugin:@typescript-eslint/recommended-requiring-type-checking",
 				"@vue/typescript/recommended",
 			],
 			parserOptions: {

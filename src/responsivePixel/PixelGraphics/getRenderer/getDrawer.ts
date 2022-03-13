90c54560f6d4b3abe6f5dd502905ca24a16696db
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-13 01:44:39 +0100

    Refactoring

--- src/responsivePixel/PixelGraphics/getRenderer/getDrawer.ts
+++ src/responsivePixel/PixelGraphics/getRenderer/getDrawer.ts
@@ -6,7 +6,7 @@ import { Render } from "../types";
 export const getDrawer = (pixelGraphics: PixelGraphics, renderList: ReadonlyArray<Render>) => {
 	const drawingTool = new DrawingTools(pixelGraphics);
 	const canvasTool = new drawingTool.Obj().create({ list: renderList });
-	return function drawer(countW, countH) {
+	return (countW, countH) => {
 		const pixelArray = getPixelArray(countW, countH);
 		drawingTool.init(countW, countH, pixelArray);
 		canvasTool.draw();

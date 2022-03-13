177e8dc68482c61027f11cfc2a1943a5dc0c4063
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-13 02:00:08 +0100

    Move function to new file

--- src/responsivePixel/PixelGraphics/getRenderer/DrawingTools.ts
+++ src/responsivePixel/PixelGraphics/getRenderer/DrawingTools.ts
@@ -1,28 +1,6 @@
 import { getObj } from "./getObj";
+import { getSeed } from "./getSeed";
 import { PixelSetter } from "./PixelSetter";
-
-const getSeed = (getRandom) => {
-	const getSeed = getRandom().seed;
-	let count = 0;
-	const i = [];
-
-	return {
-		reset() {
-			let l = count;
-			while (l--) {
-				i[l] = 0;
-			}
-		},
-		get(j) {
-			const seed = j || getSeed();
-			const nr = (count += 1);
-
-			return function () {
-				return getRandom(seed + i[nr]++ || 0);
-			};
-		},
-	};
-};
 
 class DrawingTools {
 	constructor(args) {

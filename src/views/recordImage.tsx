f264c906583fc923eed6d16dc367c2c975e0d4db
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-14 01:31:41 +0100

    Add image "brothers"

--- src/views/recordImage.tsx
+++ src/views/recordImage.tsx
@@ -1,18 +1,25 @@
 import { ImageFunction } from "../responsivePixel/PixelGraphics/types";
 
 export const recordImage: Record<
-	string, { niceName: string; getImage: () => Promise<{ default: ImageFunction; }>; }
+	string,
+	{ niceName: string; getImage: () => Promise<{ default: ImageFunction }> }
 > = {
-	teiresias: {
-		niceName: "Teiresias",
-		getImage: () => {
-			return import("../responsivePixel/scripts/teiresias");
-		},
-	},
 	graien: {
 		niceName: "Graien",
 		getImage: () => {
 			return import("../responsivePixel/scripts/graien");
+		},
+	},
+	brothers: {
+		niceName: "Brothers",
+		getImage: () => {
+			return import("../responsivePixel/scripts/brothers");
+		},
+	},
+	teiresias: {
+		niceName: "Teiresias",
+		getImage: () => {
+			return import("../responsivePixel/scripts/teiresias");
 		},
 	},
 };

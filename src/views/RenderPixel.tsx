885071e803cb34a460b41d92ad24f858078fa592
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-24 23:58:55 +0100

    Move functions into component

--- src/views/RenderPixel.tsx
+++ src/views/RenderPixel.tsx
@@ -1,26 +1,71 @@
 import "./RenderPixel.css";
-import { getRenderOnCanvas } from "./getRenderOnCanvas";
-import { useEffect, useState } from "react";
+import { useState, useRef, TouchEvent, MouseEvent, useEffect } from "react";
 import { recordImage, listPairImage } from "./recordImage";
 import { useNavigate } from "react-router-dom";
+import { getDimensionX, getDimensionY, getSizeX } from "./getDimension";
+import { PixelGraphics } from "../responsivePixel/PixelGraphics";
+import { ImageFunction } from "../responsivePixel/PixelGraphics/types";
 
 export default (props: { idImage: string }) => {
-	const {
-		width,
-		height,
-		widthAbs,
-		pixelSize,
-		isResizeable,
-		canvas,
-		setWidth,
-		setHeight,
-		setPixelSize,
-		setIsResizeable,
-		onDrag,
-		redraw,
-		setImageFunction,
-		isReady,
-	} = getRenderOnCanvas();
+	const [width, setWidth] = useState(1);
+	const [height, setHeight] = useState(1);
+	const [pixelSize, setPixelSize] = useState(5);
+	const [isResizeable, setIsResizeable] = useState(true);
+	const [pixelGraphic, setPixelGraphic] = useState<PixelGraphics | null>(
+		null
+	);
+	const [boundingClientRectCanvas, setBoundingClientRectCanvas] =
+		useState<DOMRect | null>(null);
+	const [imageFunction, setImageFunction] = useState<ImageFunction | null>(
+		null
+	);
+	const [widthAbs, setWidthAbs] = useState<number | null>(null);
+	const [isReady, setIsReady] = useState(false);
+	const canvas = useRef<HTMLCanvasElement>(null);
+
+	const redraw = () => {
+		if (pixelGraphic === null || boundingClientRectCanvas === null) {
+			return;
+		}
+		pixelGraphic.redraw({
+			widthFactor: width,
+			heightFactor: height,
+			pixelSize: pixelSize,
+			sizeX: boundingClientRectCanvas.width,
+			sizeY: boundingClientRectCanvas.height,
+		});
+	};
+
+	const onDrag = (event: MouseEvent | TouchEvent) => {
+		if (
+			isResizeable === false ||
+			("touches" in event && event.touches.length > 1) ||
+			boundingClientRectCanvas === null
+		) {
+			return;
+		}
+		event.preventDefault();
+
+		setWidthAbs(getSizeX(boundingClientRectCanvas));
+
+		setWidth(getDimensionX(event, boundingClientRectCanvas));
+		setHeight(getDimensionY(event, boundingClientRectCanvas));
+	};
+
+	useEffect(() => {
+		if (canvas.current === null || imageFunction === null) {
+			return;
+		}
+		setBoundingClientRectCanvas(canvas.current.getBoundingClientRect());
+		setPixelGraphic(
+			new PixelGraphics({
+				divCanvas: canvas.current,
+				pixelSize: pixelSize,
+				imageFunction: imageFunction,
+			})
+		);
+		setIsReady(true);
+	}, [canvas, imageFunction]);
 
 	const setPixelCount = (pixelCount) => {
 		setPixelSize((pixelCountMax ?? 1) / pixelCount);

8a7127836939abddabe1af32da163c0ba45b6356
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-25 00:16:29 +0100

    Add `tailwind`

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "index.html"],
+	darkMode: "media",
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Graphik", "sans-serif"],
+				serif: ["Merriweather", "serif"],
+			},
+		},
+	},
+};

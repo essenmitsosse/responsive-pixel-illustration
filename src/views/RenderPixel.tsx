4414a6a67ebf8c3bfc5687e9efd4a5d21c49b455
Marcus Blättermann <marcus@essenmitsosse.de>
2022-03-25 01:34:17 +0100

    Add basic styling

--- src/views/RenderPixel.tsx
+++ src/views/RenderPixel.tsx
@@ -98,88 +98,110 @@ export default (props: { idImage: string }) => {
 
 	return (
 		<div className="home">
-			<label style={{ display: "inline-block" }}>
-				<span>Select Image</span>
-				<select
-					style={{ display: "block" }}
-					value={props.idImage}
-					onChange={(event) => setIdImage(event.currentTarget.value)}
-				>
-					{listPairImage.map(([id, image]) => (
-						<option value={id} key={id}>
-							{image.niceName}
-						</option>
-					))}
-				</select>
-			</label>
-			<label style={{ display: "inline-block" }}>
-				<span>Width</span>
-				<input
-					style={{ display: "block" }}
-					value={width}
-					onInput={(event) =>
-						setWidth(parseFloat(event.currentTarget.value))
-					}
-					type="range"
-					min="0"
-					max="1"
-					step="0.0001"
-				/>{" "}
-			</label>
-			<label style={{ display: "inline-block" }}>
-				<span>Height</span>
-				<input
-					style={{ display: "block" }}
-					value={height}
-					onInput={(event) =>
-						setHeight(parseFloat(event.currentTarget.value))
-					}
-					type="range"
-					min="0"
-					max="1"
-					step="0.0001"
-				/>
-			</label>
-			<label style={{ display: "inline-block" }}>
-				<span>Pixel Size ({Math.round(pixelSize)})</span>
-				<input
-					style={{ display: "block" }}
-					value={pixelSize}
-					onInput={(event) =>
-						setPixelSize(parseFloat(event.currentTarget.value))
-					}
-					type="range"
-					min="2"
-					max="30"
-					step="1"
-				/>
-			</label>
-			<label style={{ display: "inline-block" }}>
-				<span>
-					Pixel Count ({Math.round(pixelCount)} /{" "}
-					{Math.round(pixelCountMax ?? 1)})
-				</span>
-				<input
-					style={{ display: "block" }}
-					value={pixelCount}
-					onInput={(event) =>
-						setPixelCount(parseFloat(event.currentTarget.value))
-					}
-					type="range"
-					min={pixelCountMin}
-					max={pixelCountMax ?? 1}
-					step="1"
-				/>
-			</label>
-			<label style={{ display: "inline-block" }}>
-				<span>Resize on Hover</span>
-				<input
-					style={{ display: "block" }}
-					checked={isResizeable}
-					onChange={() => setIsResizeable(!isResizeable)}
-					type="checkbox"
-				/>
-			</label>
+			<form className="mb-4 flex w-full flex-wrap">
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Select Image
+					</span>
+					<div className="relative">
+						<select
+							className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-1 px-4 pr-8  focus:border-gray-800 focus:outline-none dark:border-gray-600 dark:bg-gray-700 focus:dark:border-gray-300"
+							value={props.idImage}
+							onChange={(event) =>
+								setIdImage(event.currentTarget.value)
+							}
+						>
+							{listPairImage.map(([id, image]) => (
+								<option value={id} key={id}>
+									{image.niceName}
+								</option>
+							))}
+						</select>
+					</div>
+				</label>
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Width
+					</span>
+					<input
+						className="h-0.5 w-full appearance-none rounded bg-gray-700 dark:bg-gray-300"
+						value={width}
+						onInput={(event) =>
+							setWidth(parseFloat(event.currentTarget.value))
+						}
+						type="range"
+						min="0"
+						max="1"
+						step="0.0001"
+					/>{" "}
+				</label>
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Height
+					</span>
+					<input
+						className="h-0.5 w-full appearance-none rounded bg-gray-700 dark:bg-gray-300"
+						value={height}
+						onInput={(event) =>
+							setHeight(parseFloat(event.currentTarget.value))
+						}
+						type="range"
+						min="0"
+						max="1"
+						step="0.0001"
+					/>
+				</label>
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Pixel Size{" "}
+						<span className="opacity-50">
+							({Math.round(pixelSize)})
+						</span>
+					</span>
+					<input
+						className="h-0.5 w-full appearance-none rounded bg-gray-700 dark:bg-gray-300 "
+						value={pixelSize}
+						onInput={(event) =>
+							setPixelSize(parseFloat(event.currentTarget.value))
+						}
+						type="range"
+						min="2"
+						max="30"
+						step="1"
+					/>
+				</label>
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Pixel Count{" "}
+						<span className="opacity-50">
+							({Math.round(pixelCount)} /{" "}
+							{Math.round(pixelCountMax ?? 1)})
+						</span>
+					</span>
+					<input
+						className="h-0.5 w-full appearance-none rounded bg-gray-700 dark:bg-gray-300"
+						value={pixelCount}
+						onInput={(event) =>
+							setPixelCount(parseFloat(event.currentTarget.value))
+						}
+						type="range"
+						min={pixelCountMin}
+						max={pixelCountMax ?? 1}
+						step="1"
+					/>
+				</label>
+				<label className="inline-block w-1/6 px-4">
+					<span className="inline-block pb-2 text-xs font-bold uppercase tracking-wide">
+						Resize on Hover
+					</span>
+					<input
+						className="form-checkbox mt-2 block h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-300 bg-gray-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none dark:border-gray-600 dark:bg-gray-700"
+						checked={isResizeable}
+						onChange={() => setIsResizeable(!isResizeable)}
+						type="checkbox"
+					/>
+				</label>
+			</form>
 			{isReady ? null : "Bild lädt ..."}
 			<div className="wrapper-canvas">
 				<canvas

1349be9d0d3ef22bb154416b76043a6d040dbbb4
Marcus Blättermann <marcus@essenmitsosse.de>
2024-07-04 17:37:50 +0200

    Update TSConfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import Image from './views/Image'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Image idImage="tantalos" />} />
-          <Route path=":idImage" element={<Image />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const $root = document.getElementById('root')
+
+if ($root !== null) {
+  createRoot($root).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Image idImage="tantalos" />} />
+            <Route path=":idImage" element={<Image />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}

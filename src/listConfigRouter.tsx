4593dce95dc2a68e75ea2386b811ab93b216ecb2
Marcus Blättermann <marcus@essenmitsosse.de>
2024-07-11 22:30:36 +0200

    Dynamically load images via React Router

--- src/listConfigRouter.tsx
+++ src/listConfigRouter.tsx
@@ -1,9 +1,41 @@
-import { RouteObject } from 'react-router-dom'
+import type { RouteObject } from 'react-router-dom'
+import type { ImageFunction } from './responsivePixel/PixelGraphics/types'
 import Image from './views/Image'
 
+export type LoaderDataPixel = {
+  promiseImageFunction: Promise<ImageFunction>
+  idImage?: string
+}
+
+const makeGetImageFunction = async (idImage: string) =>
+  (await import(`./responsivePixel/images/${idImage}.ts`)).default
+
+const loaderIdImage = (args: { params: { idImage?: string } }) => {
+  const idImage = args.params.idImage ?? 'tantalos'
+  return { idImage }
+}
+
+const loaderImage = async (args: {
+  params: { idImage?: string }
+}): Promise<LoaderDataPixel> => {
+  const idImage = args.params.idImage ?? 'tantalos'
+  return {
+    promiseImageFunction: makeGetImageFunction(idImage),
+    idImage: loaderIdImage(args).idImage,
+  }
+}
+
 const listConfigRouter: Array<RouteObject> = [
-  { index: true, element: <Image idImage="tantalos" /> },
-  { path: ':idImage', element: <Image /> },
+  {
+    index: true,
+    element: <Image />,
+    loader: loaderImage,
+  },
+  {
+    path: ':idImage',
+    element: <Image />,
+    loader: loaderImage,
+  },
 ]
 
 export default listConfigRouter
--- src/views/Image.tsx
+++ src/views/Image.tsx
@@ -1,9 +1,22 @@
-import { useParams } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 import RenderPixel from './RenderPixel'
+import type { LoaderDataPixel } from '../listConfigRouter'
+import type { ImageFunction } from 'src/responsivePixel/PixelGraphics/types'
+import { useEffect, useState } from 'react'
 
-export default (props: { idImage?: string }) => {
-  const params = useParams()
-  return (
-    <RenderPixel idImage={params.idImage ?? props.idImage ?? 'teiresias'} />
-  )
+export default () => {
+  const data = useLoaderData() as LoaderDataPixel
+  const [imageFunction, setImageFunction] = useState<ImageFunction | null>(null)
+
+  useEffect(() => {
+    setImageFunction(null)
+    async function loadImage() {
+      const imageFunction = await data.promiseImageFunction
+      setImageFunction(imageFunction)
+    }
+
+    loadImage()
+  }, [data.promiseImageFunction])
+
+  return <RenderPixel imageFunction={imageFunction} idImage={data.idImage} />
 }

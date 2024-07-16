00e2a04da068cb7c32d8a42ba94b6d5744e0716c
Marcus Blättermann <marcus@essenmitsosse.de>
2024-07-16 16:46:47 +0200

    Add tests to "Table Comic"

--- src/responsivePixel/images/test.cy.tsx
+++ src/responsivePixel/images/test.cy.tsx
@@ -6,6 +6,7 @@ import graien from './graien'
 import sparta from './sparta'
 import sphinx from './sphinx'
 import stripes from './stripes'
+import tableComic from './tableComic'
 import tantalos from './tantalos'
 import teiresias from './teiresias'
 import trex from './trex'
@@ -56,6 +57,36 @@ DO_SCREENSHOT_LIST_IMAGE({
     { SIZE_X: 60, SIZE_Y: 120 },
     { SIZE_X: 100, SIZE_Y: 60 },
   ],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 1 }),
+  NAME: 'table-comic',
+  LIST_SIZE: [{ SIZE_X: 200, SIZE_Y: 200 }],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 2 }),
+  NAME: 'table-comic',
+  LIST_SIZE: [{ SIZE_X: 120, SIZE_Y: 250 }],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 3 }),
+  NAME: 'table-comic',
+  LIST_SIZE: [{ SIZE_X: 300, SIZE_Y: 190 }],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 1, altVersion: true }),
+  NAME: 'table-comic-alt',
+  LIST_SIZE: [{ SIZE_X: 200, SIZE_Y: 200 }],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 2, altVersion: true }),
+  NAME: 'table-comic-alt',
+  LIST_SIZE: [{ SIZE_X: 120, SIZE_Y: 250 }],
+})
+DO_SCREENSHOT_LIST_IMAGE({
+  IMAGE_FUNCTION: tableComic.getImageFunction({ id: 3, altVersion: true }),
+  NAME: 'table-comic-alt',
+  LIST_SIZE: [{ SIZE_X: 300, SIZE_Y: 190 }],
 })
 DO_SCREENSHOT_LIST_IMAGE({ IMAGE_FUNCTION: tantalos, NAME: 'tantalos' })
 DO_SCREENSHOT_LIST_IMAGE({ IMAGE_FUNCTION: teiresias, NAME: 'teiresias' })

877adda3a89de9eaf7f68e40b916919ca63d9cc6
Marcus Blättermann <marcus@essenmitsosse.de>
2024-11-24 17:59:05 +0100

    Add screenshots for each image

--- tests/screenshots.spec.js
+++ tests/screenshots.spec.js
@@ -1,8 +1,37 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto("/");
-	await page.locator('strong:text("Tantalos")').click();
-	await expect(page).toHaveScreenshot("tantalos.png");
+});
+
+const listScreenshots = [
+	{ name: "graien", niceName: "The Three Graeae" },
+	{ name: "tantalos", niceName: "Tantalos" },
+	{ name: "teiresias", niceName: "Teiresias" },
+	{ name: "brothers", niceName: "Brothers" },
+	{ name: "zeus", niceName: "Zeus" },
+	{ name: "argos", niceName: "The Argos" },
+	{ name: "sphinx", niceName: "The Sphinx" },
+	{ name: "letter", niceName: "Letter" },
+	{ name: "persons_lessrandom", niceName: "Trees" },
+	{ name: "persons_lessrandom", niceName: "Persons" },
+	{ name: "panels", niceName: "Panels" },
+	{ name: "turnaround", niceName: "Turnaround" },
+	{ name: "table2", niceName: "Comic 2" },
+	{ name: "relativity", niceName: "Relativity" },
+	{ name: "stripes-1", niceName: "Stripe1" },
+	{ name: "stripes-2", niceName: "Stripe2" },
+	{ name: "landscape", niceName: "Landscape" },
+	{ name: "sparta", niceName: "Sparta" },
+	{ name: "trex", niceName: "T-Rex" },
+	{ name: "typo", niceName: "Typo" },
+	{ name: "random-distribution", niceName: "Random" },
+];
+
+listScreenshots.forEach(({ name, niceName }) => {
+	test(niceName, async ({ page }) => {
+		await page.locator(`strong:text("${niceName}")`).click();
+		await expect(page).toHaveScreenshot(`${name}.png`);
+	});
 });

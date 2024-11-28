ab9440c168ee9782082389422821d875ddc27351
Marcus Blättermann <marcus@essenmitsosse.de>
2024-11-28 22:03:20 +0100

    Move imports into main file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { inject } from "@vercel/analytics";
+import "./renderengine/init.js";
+import "./renderengine/info.js";
+import "./renderengine/helper.js";
+import "./renderengine/pixel.js";
+import "./renderengine/creator.js";
+import "./renderengine/renderer.js";
+import "./renderengine/admin.js";
 
 inject();
 

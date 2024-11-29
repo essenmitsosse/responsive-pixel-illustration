d7e46e3dc3f928a094292369549e82e759bb27f5
Marcus Blättermann <marcus@essenmitsosse.de>
2024-11-29 11:20:15 +0100

    Add basic ESLint setup

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import listConfigBaseTypescript from "./eslint/listConfigBaseTypescript.js";
+
+/** @type {ReadonlyArray<import('eslint').Linter.Config>} */
+const listConfigEslint = [
+	...listConfigBaseTypescript,
+
+	/** Global ignores */
+	{
+		ignores: ["tsbuild", "dist"],
+	},
+];
+
+export default listConfigEslint;
---
+++ eslint/listConfigBaseTypescript.js
@@ -0,0 +1,37 @@
+import tsParser from "@typescript-eslint/parser";
+
+/**
+ * Basic setup to make sure TypeScript files can be linted and types are
+ * available to the linting rules
+ */
+/** @type {ReadonlyArray<import('eslint').Linter.Config>} */
+const listConfigSetupTypescript = [
+	{
+		files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaFeatures: { modules: true },
+				ecmaVersion: "latest",
+				project: "tsconfig.json",
+			},
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: true,
+		},
+		settings: {
+			"import/parsers": {
+				"@typescript-eslint/parser": [
+					".ts",
+					".tsx",
+					".js",
+					".cjs",
+					".mjs",
+					".jsx",
+				],
+			},
+		},
+	},
+];
+
+export default listConfigSetupTypescript;

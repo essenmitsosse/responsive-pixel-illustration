9cd561f33bc6de72a111d5254c21e6471cb03e84
Marcus Blättermann <marcus@essenmitsosse.de>
2024-11-30 18:27:16 +0100

    Enable ESLint rule `no-constant-binary-expression`

--- eslint/listConfigBase.js
+++ eslint/listConfigBase.js
@@ -8,8 +8,6 @@ const listConfigEslintBase = [
     rules: {
       ...js.configs.recommended.rules,
 
-      'no-constant-binary-expression': 'off',
-
       'no-empty': 'off',
 
       'no-redeclare': 'off',

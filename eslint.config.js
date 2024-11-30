01593135fbbadc570a3c495fb5a254eb28277456
Marcus Blättermann <marcus@essenmitsosse.de>
2024-11-30 18:16:30 +0100

    Enable all non-violated recommended ESLint rules
    In the next commit the disabled rules will be enabled one by one, fixing the
    violations in the codebase.

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
+import listConfigBase from './eslint/listConfigBase.js'
 import listConfigBaseTypescript from './eslint/listConfigBaseTypescript.js'
 
 /** @type {ReadonlyArray<import('eslint').Linter.Config>} */
 const listConfigEslint = [
   ...listConfigBaseTypescript,
+
+  ...listConfigBase,
 
   /** Global ignores */
   {
---
+++ eslint/listConfigBase.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+/** @type {ReadonlyArray<import('eslint').Linter.Config>} */
+const listConfigEslintBase = [
+  {
+    languageOptions: { globals: { ...globals.browser, process: true } },
+    rules: {
+      ...js.configs.recommended.rules,
+
+      'no-cond-assign': 'off',
+
+      'no-constant-binary-expression': 'off',
+
+      'no-empty': 'off',
+
+      'no-redeclare': 'off',
+
+      'no-sparse-arrays': 'off',
+
+      'no-unused-vars': 'off',
+    },
+  },
+
+  /** Separate config for config files (which have access to a Node env) */
+  {
+    files: ['*.config.*'],
+    languageOptions: { globals: { ...globals.node, process: true } },
+  },
+]
+
+export default listConfigEslintBase

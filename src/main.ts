2c6516db39d6c6519e765939ef94dfbb21384747
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-01 02:19:23 +0100

    Fix violation of TypeScript ESLint rule `explicit-function-return-type`

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { inject } from '@vercel/analytics'
 import { InitPixel } from '@/renderengine/init'
 
 inject()
-;(() =>
+;((): InitPixel =>
   new InitPixel({
     div: document.getElementById('main'),
     // imageName: window.location.hash.substr(1)

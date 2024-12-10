4e5986a0be12bc0ad80415a447c737495abd5234
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-10 23:52:17 +0100

    Add ESLint rule for consistant new lines
    Yes, I am that obsessed with deterministic styling

--- eslint/listConfigStylistic.js
+++ eslint/listConfigStylistic.js
@@ -6,6 +6,32 @@ const listConfigStylistic = [
     plugins: { '@stylistic/ts': stylistic },
     rules: {
       /**
+       * This rule requires or disallows blank lines between the given 2 kinds
+       * of statements. Properly blank lines help developers to understand the
+       * code.
+       * {@link https://eslint.style/rules/js/padding-line-between-statements}
+       */
+      '@stylistic/ts/padding-line-between-statements': [
+        'error',
+        /** No new line between expression */
+        { blankLine: 'always', next: 'expression', prev: 'expression' },
+
+        /** New line before return statement */
+        { blankLine: 'always', next: 'return', prev: '*' },
+
+        /** New line around block-like */
+        { blankLine: 'always', next: 'block-like', prev: '*' },
+        { blankLine: 'always', next: '*', prev: 'block-like' },
+
+        /** No new line before between const */
+        { blankLine: 'always', next: 'const', prev: '*' },
+        { blankLine: 'always', next: '*', prev: 'const' },
+
+        /** New line before export */
+        { blankLine: 'always', next: 'export', prev: '*' },
+      ],
+
+      /**
        * Enforce the consistent use of either backticks, double, or single
        * quotes. This doubles with the Prettier rule, but also removes
        * unnesssary backticks.

essenmitsosse/responsive-pixel-illustration

commit c3dc1ff5753101929e600bb64c26a53de0df3ffc

Remove unnecessary reassignment

src/scripts/tableComic2/tablecomic-1.js
@@ -1,10 +1,9 @@
 // BEGINN getStrip /\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-/\-
 export const getFace = function () {
   var backgroundColor = [0, 0, 0],
-    basic = this.basic,
     that = this,
     getPanel = function () {
-      var actor = new basic.Actor({
+      var actor = new this.basic.Actor({
           color: [that.rInt(0, 255), that.rInt(0, 255), that.rInt(0, 255)],
         }),
         pupilS = { map: 'c', min: 0.5, max: 0.8 },
@@ -17,10 +16,10 @@ export const getFace = function () {
         },
         list: [
           {
-            what: new basic.Stage(),
+            what: new this.basic.Stage(),
           },
           {
-            what: new basic.Head({
+            what: new this.basic.Head({
               actor,
               baseSX_: 1,
               noHat: true,
@@ -77,7 +76,6 @@ export const getFace = function () {
 export const getStrip = function getStrip() {
   var stripInfo = new this.getStripInfo(),
     basicPanels = stripInfo.panels,
-    length = basicPanels.length,
     count = 0,
     currentPanel,
     finalPanels = [],
@@ -87,7 +85,7 @@ export const getStrip = function getStrip() {
       stripInfo.inventory,
     )
 
-  while (count < length) {
+  while (count < basicPanels.length) {
     currentPanel = basicPanels[count]
 
     if (currentPanel.draw === 'faceDraw') {

src/renderengine/admin.js
@@ -1,6 +1,4 @@
 export const Admin = function (args) {
-  var body = args.body
-
   this.pixel = args.pixel
 
   this.getClicker = this.getClickerGetter(this.pixel)
@@ -10,7 +8,10 @@ export const Admin = function (args) {
   this.admin = args.admin
 
   // Setup Basic Showcase/Admin Layout
-  body.setAttribute('class', (body.getAttribute('class') || '') + ' showcase')
+  args.body.setAttribute(
+    'class',
+    (args.body.getAttribute('class') || '') + ' showcase',
+  )
 
   this.mainAdmin = document.createElement('div')
 
@@ -36,7 +37,7 @@ export const Admin = function (args) {
 
   this.setupSlider()
 
-  body.appendChild(this.mainAdmin)
+  args.body.appendChild(this.mainAdmin)
 
   this.mainAdmin.setAttribute('id', 'mainAdmin')
 }
@@ -186,18 +187,14 @@ Admin.prototype.setupSlider = function () {
 }
 
 Admin.prototype.getSliderControlGetter = function () {
-  var pixel = this.pixel,
-    lastSliderParent,
-    lastValueName
+  var lastSliderParent,
+    lastValueName,
+    that = this
 
   return {
     slider: function getSliderControl(slider, span, args) {
       var value,
-        parentNode = slider.parentNode.parentNode,
-        valueName = args.valueName,
-        min = args.input.min,
-        max = args.input.max,
-        diff = max - min,
+        diff = args.input.max - args.input.min,
         outputMap = args.output || { min: 0, max: 1 },
         outputMin = outputMap.min,
         outputFactor = (outputMap.max - outputMin) / diff,
@@ -206,7 +203,7 @@ Admin.prototype.getSliderControlGetter = function () {
 
           span.setAttribute(
             'style',
-            'left: ' + (((value - min) / diff) * 100 - 10) + '%;',
+            'left: ' + (((value - args.input.min) / diff) * 100 - 10) + '%;',
           )
         },
         update = function (setValue, single) {
@@ -215,16 +212,17 @@ Admin.prototype.getSliderControlGetter = function () {
           // If update is received with a sepcific value (e.g. from server), than just update the visual slider
           if (typeof setValue === 'number') {
             // obj[ valueName ] = setValue;
-            value = slider.value = (setValue - outputMin) / outputFactor + min
+            value = slider.value =
+              (setValue - outputMin) / outputFactor + args.input.min
 
             if (single) {
-              if (lastValueName !== valueName && lastSliderParent) {
+              if (lastValueName !== args.valueName && lastSliderParent) {
                 lastSliderParent.setAttribute('style', '')
               }
 
-              lastValueName = valueName
+              lastValueName = args.valueName
 
-              lastSliderParent = parentNode
+              lastSliderParent = slider.parentNode.parentNode
             }
 
             updateInfoSpan()
@@ -237,9 +235,10 @@ Admin.prototype.getSliderControlGetter = function () {
 
             updateInfoSpan()
 
-            obj[valueName] = (value - min) * outputFactor + outputMin
+            obj[args.valueName] =
+              (value - args.input.min) * outputFactor + outputMin
 
-            pixel.sliderChange(obj)
+            that.pixel.sliderChange(obj)
           }
         }
 
@@ -254,7 +253,6 @@ Admin.prototype.getSliderControlGetter = function () {
 
     number: function getButtonControl(number, args) {
       var value,
-        valueName = args.valueName,
         update = function (setValue, dontForce) {
           var obj = {}
 
@@ -263,12 +261,12 @@ Admin.prototype.getSliderControlGetter = function () {
           } else if (value !== number.value * 1) {
             value = number.value * 1
 
-            obj[valueName] = value
+            obj[args.valueName] = value
 
             if (!dontForce && args.forceRedraw) {
-              pixel.changeForceRedraw(obj)
+              that.pixel.changeForceRedraw(obj)
             } else {
-              pixel.sliderChange(obj)
+              that.pixel.sliderChange(obj)
             }
           }
         }
@@ -316,7 +314,7 @@ Admin.prototype.setupBasicControls = function (hasRandom) {
 }
 
 Admin.prototype.getButtonCreater = function (div) {
-  var pixel = this.pixel
+  var that = this
 
   return function createButton(args) {
     var button = div.addMessage(
@@ -326,11 +324,11 @@ Admin.prototype.getButtonCreater = function (div) {
         return function () {
           pixel[args.functionName](args.args)
         }
-      })(pixel),
+      })(that.pixel),
     )
 
     if (args.callback) {
-      pixel[args.callback](button)
+      that.pixel[args.callback](button)
     }
   }
 }

src/renderengine/creator.js
@@ -166,9 +166,6 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   this.Primitive.prototype.getName = 'Primitive'
 
   this.Primitive.prototype.create = (function () {
-    var setColorArray = that.pixelSetter.setColorArray,
-      setColorMask = that.pixelSetter.setColorMask
-
     return function (args, inherit) {
       inherit = inherit || {}
 
@@ -234,7 +231,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       }
 
       if (args.mask) {
-        newArgs.mask = setColorMask
+        newArgs.mask = that.pixelSetter.setColorMask
       }
 
       newArgs.zInd = (inherit.zInd || 0) + (args.z || 0)
@@ -242,7 +239,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       if (args.list) {
         newArgs.list = args.list
       } else {
-        this.getColorArray = setColorArray(
+        this.getColorArray = that.pixelSetter.setColorArray(
           newArgs.color,
           newArgs.clear,
           newArgs.zInd,
@@ -356,8 +353,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
     rotate,
   ) {
     var newPoints = [],
-      points = args.points,
-      l = points.length
+      l = args.points.length
 
     reflectX = (args.rX || false) !== reflectX
 
@@ -365,7 +361,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
 
     while (l--) {
       newPoints.push(
-        new pixelUnit.Position(points[l], reflectX, reflectY, rotate),
+        new pixelUnit.Position(args.points[l], reflectX, reflectY, rotate),
       )
     }
 
@@ -376,73 +372,71 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   }
 
   this.Line.prototype.draw = (function () {
-    var abs = Math.abs,
-      getDrawLine = function (set) {
-        return function (p0, p1) {
-          // Draw a single Lines
-          var x0, y0, x1, y1, dx, dy, sy, err, e2
+    var getDrawLine = function (set) {
+      return function (p0, p1) {
+        // Draw a single Lines
+        var x0, y0, x1, y1, dx, dy, sy, err, e2
 
-          if (isNaN(p0.x) || isNaN(p0.y) || isNaN(p1.x) || isNaN(p1.y)) {
-            throw new Error('Line with NaN found!', p0.x, p0.y, p1.x, p1.y)
+        if (isNaN(p0.x) || isNaN(p0.y) || isNaN(p1.x) || isNaN(p1.y)) {
+          throw new Error('Line with NaN found!', p0.x, p0.y, p1.x, p1.y)
+        }
+
+        if (p0.x > p1.x) {
+          x1 = p0.x
+
+          y1 = p0.y
+
+          x0 = p1.x
+
+          y0 = p1.y
+        } else {
+          x0 = p0.x
+
+          y0 = p0.y
+
+          x1 = p1.x
+
+          y1 = p1.y
+        }
+
+        dx = Math.abs(x1 - x0)
+
+        dy = -Math.abs(y1 - y0)
+
+        sy = y0 < y1 ? 1 : -1
+
+        err = dx + dy
+
+        while (true) {
+          set(x0, y0)
+
+          if (x0 === x1 && y0 === y1) {
+            return p1
           }
 
-          if (p0.x > p1.x) {
-            x1 = p0.x
+          e2 = 2 * err
 
-            y1 = p0.y
+          if (e2 > dy) {
+            err += dy
 
-            x0 = p1.x
-
-            y0 = p1.y
-          } else {
-            x0 = p0.x
-
-            y0 = p0.y
-
-            x1 = p1.x
-
-            y1 = p1.y
+            x0 += 1
           }
 
-          dx = abs(x1 - x0)
+          if (e2 < dx) {
+            err += dx
 
-          dy = -abs(y1 - y0)
-
-          sy = y0 < y1 ? 1 : -1
-
-          err = dx + dy
-
-          while (true) {
-            set(x0, y0)
-
-            if (x0 === x1 && y0 === y1) {
-              return p1
-            }
-
-            e2 = 2 * err
-
-            if (e2 > dy) {
-              err += dy
-
-              x0 += 1
-            }
-
-            if (e2 < dx) {
-              err += dx
-
-              y0 += sy
-            }
+            y0 += sy
           }
         }
       }
+    }
 
     return function () {
       // Draw all Lines
-      var args = this.args,
-        p = args.points,
-        l = args.LineCount,
+      var p = this.args.points,
+        l = this.args.LineCount,
         nextPoint = p[l](),
-        firstPoint = args.closed ? nextPoint : false,
+        firstPoint = this.args.closed ? nextPoint : false,
         drawLine = getDrawLine(this.lineSetter())
 
       while (l--) {
@@ -461,8 +455,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   this.Polygon.prototype.getName = 'Polygon'
 
   this.Polygon.prototype.draw = (function () {
-    var abs = Math.abs,
-      getLineEdgeGetter = function (edgeList) {
+    var getLineEdgeGetter = function (edgeList) {
         var i = -1
 
         return function (p0, p1) {
@@ -493,9 +486,9 @@ export const DrawingTools = function (pixelUnit, getRandom) {
           // 	return p1;
           // }
 
-          dx = abs(x1 - x0)
+          dx = Math.abs(x1 - x0)
 
-          dy = -abs(y1 - y0)
+          dy = -Math.abs(y1 - y0)
 
           sy = y0 < y1 ? 1 : -1
 
@@ -552,13 +545,9 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       },
       getDrawRow = function (set) {
         return function (p0, p1) {
-          var x0 = p0.x0,
-            x1 = p1.x1,
-            y = p1.y
-
           do {
-            set(x0, y)
-          } while ((x0 += 1) <= x1)
+            set(p0.x0, p1.y)
+          } while ((p0.x0 += 1) <= p1.x1)
         }
       },
       sortFunction = function (a, b) {
@@ -573,13 +562,12 @@ export const DrawingTools = function (pixelUnit, getRandom) {
 
     return function () {
       // Draw all Lines
-      var args = this.args,
-        edgeList = [],
+      var edgeList = [],
         colorArraySet = this.getColorArray(),
         drawRow = getDrawRow(colorArraySet),
         getLineEdge = getLineEdgeGetter(edgeList),
-        p = args.points,
-        l = args.LineCount,
+        p = this.args.points,
+        l = this.args.LineCount,
         nextPoint = p[l](true),
         firstPoint = nextPoint
 
@@ -825,20 +813,19 @@ export const DrawingTools = function (pixelUnit, getRandom) {
     }
 
     return function () {
-      var args = this.args,
-        list = this.args.list || this.list
+      var list = this.args.list || this.list
 
       if (this.args.list || this.list) {
         this.args.list = convertList(list, {
           // Things to inherit to Children
-          color: args.color,
-          clear: args.clear,
-          reflectX: args.reflectX,
-          reflectY: args.reflectY,
-          zInd: args.zInd,
-          id: args.id,
-          save: args.save,
-          rotate: args.rotate,
+          color: this.args.color,
+          clear: this.args.clear,
+          reflectX: this.args.reflectX,
+          reflectY: this.args.reflectY,
+          zInd: this.args.zInd,
+          id: this.args.id,
+          save: this.args.save,
+          rotate: this.args.rotate,
         })
       }
     }
@@ -848,9 +835,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   this.Obj.prototype.draw = (function (pixelUnit) {
     // Draws Object, consisting of other Objects and Primitives.
     return function () {
-      var args = this.args,
-        list = args.list,
-        l = list.length,
+      var l = this.args.list.length,
         dimensions = this.dimensions.calc(),
         oldMask
 
@@ -858,18 +843,18 @@ export const DrawingTools = function (pixelUnit, getRandom) {
         return
       }
 
-      if (args.mask) {
-        oldMask = args.mask(dimensions, true)
+      if (this.args.mask) {
+        oldMask = this.args.mask(dimensions, true)
       }
 
       pixelUnit.push(dimensions)
 
       while (l--) {
-        list[l].draw()
+        this.args.list[l].draw()
       }
 
-      if (args.mask) {
-        args.mask(oldMask, false)
+      if (this.args.mask) {
+        this.args.mask(oldMask, false)
       }
 
       pixelUnit.pop()
@@ -887,61 +872,63 @@ export const DrawingTools = function (pixelUnit, getRandom) {
 
   this.Stripes.prototype.detailInit = function (args) {
     var random,
-      stripes = args.stripes,
       /** Width of a single Line */
       horizontal = (this.horizontal =
-        (this.rotate ? !stripes.horizontal : stripes.horizontal) || false),
+        (this.rotate ? !args.stripes.horizontal : args.stripes.horizontal) ||
+        false),
       Dimension = horizontal ? pixelUnit.Height : pixelUnit.Width
 
     /** Width of a single Line */
-    this.stripWidth = new Dimension(stripes.strip || { a: 1 })
+    this.stripWidth = new Dimension(args.stripes.strip || { a: 1 })
 
     /** Width of a single Line */
-    this.gapWidth = new Dimension(stripes.gap || { a: 0 })
+    this.gapWidth = new Dimension(args.stripes.gap || { a: 0 })
 
-    if (stripes.strip && stripes.strip.random) {
-      this.stripWidthRandom = new pixelUnit.createSize(stripes.strip.random)
-
-      random = true
-    }
-
-    if (stripes.gap && stripes.gap.random) {
-      this.gapWidthRandom = new pixelUnit.createSize(stripes.gap.random)
+    if (args.stripes.strip && args.stripes.strip.random) {
+      this.stripWidthRandom = new pixelUnit.createSize(
+        args.stripes.strip.random,
+      )
 
       random = true
     }
 
-    if (stripes.random) {
-      if (typeof stripes.random === 'object') {
-        stripes.random.height = !horizontal
+    if (args.stripes.gap && args.stripes.gap.random) {
+      this.gapWidthRandom = new pixelUnit.createSize(args.stripes.gap.random)
+
+      random = true
+    }
+
+    if (args.stripes.random) {
+      if (typeof args.stripes.random === 'object') {
+        args.stripes.random.height = !horizontal
       }
 
-      this.lengthRandom = new pixelUnit.createSize(stripes.random)
+      this.lengthRandom = new pixelUnit.createSize(args.stripes.random)
 
       random = true
     }
 
-    if (stripes.change) {
-      if (typeof stripes.change === 'object') {
-        stripes.change.height = !horizontal
+    if (args.stripes.change) {
+      if (typeof args.stripes.change === 'object') {
+        args.stripes.change.height = !horizontal
       }
 
-      this.lengthChange = new pixelUnit.createSize(stripes.change)
+      this.lengthChange = new pixelUnit.createSize(args.stripes.change)
 
       random = true
     }
 
     if (random) {
-      this.random = that.seed.get(stripes.seed)
+      this.random = that.seed.get(args.stripes.seed)
     }
 
-    this.cut = stripes.cut
+    this.cut = args.stripes.cut
 
-    this.overflow = stripes.overflow
+    this.overflow = args.stripes.overflow
 
-    this.round = stripes.round
+    this.round = args.stripes.round
 
-    this.fromStart = stripes.fromStart
+    this.fromStart = args.stripes.fromStart
 
     this.fromOtherSide = horizontal ? this.fromBottom : this.fromRight
 
@@ -986,21 +973,15 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   }
 
   this.Stripes.prototype.draw = function () {
-    var args = this.args,
-      dimensions = this.dimensions.calc()
+    var dimensions = this.dimensions.calc()
 
     if (dimensions.checkMin()) {
       return
     }
 
-    var fromRight = this.fromRight,
-      fromBottom = this.fromBottom,
-      horizontal = this.horizontal,
-      fromOtherSide = this.fromOtherSide,
-      cut = this.cut,
-      stripWidth = this.stripWidth.getReal(),
+    var stripWidth = this.stripWidth.getReal(),
       gapWidth = this.gapWidth.getReal(),
-      size = horizontal ? dimensions.height : dimensions.width,
+      size = this.horizontal ? dimensions.height : dimensions.width,
       singleSX = gapWidth + stripWidth
 
     if (this.round) {
@@ -1016,31 +997,45 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       lengthChangeStep =
         (lengthChange /
           (this.horizontal ? dimensions.height : dimensions.width)) *
-        (fromOtherSide ? -1 : 1),
-      totalHeightChange = Math.round(fromOtherSide ? -lengthChangeStep : 0),
+        (this.fromOtherSide ? -1 : 1),
+      totalHeightChange = Math.round(
+        this.fromOtherSide ? -lengthChangeStep : 0,
+      ),
       startX =
         dimensions.posX -
-        (horizontal && fromRight && fromBottom ? lengthChange : 0),
+        (this.horizontal && this.fromRight && this.fromBottom
+          ? lengthChange
+          : 0),
       startY =
         dimensions.posY -
-        (!horizontal && fromRight && fromBottom ? lengthChange : 0),
+        (!this.horizontal && this.fromRight && this.fromBottom
+          ? lengthChange
+          : 0),
       width =
         dimensions.width +
-        (horizontal && !fromRight && fromBottom ? lengthChange : 0) +
-        (horizontal && fromRight && fromBottom ? lengthChange : 0),
+        (this.horizontal && !this.fromRight && this.fromBottom
+          ? lengthChange
+          : 0) +
+        (this.horizontal && this.fromRight && this.fromBottom
+          ? lengthChange
+          : 0),
       height =
         dimensions.height +
-        (!horizontal && fromRight && !fromBottom ? lengthChange : 0) +
-        (!horizontal && fromRight && fromBottom ? lengthChange : 0),
+        (!this.horizontal && this.fromRight && !this.fromBottom
+          ? lengthChange
+          : 0) +
+        (!this.horizontal && this.fromRight && this.fromBottom
+          ? lengthChange
+          : 0),
       endX = startX + width,
       endY = startY + height,
       start =
-        (horizontal ? startY : startX) +
-        (fromOtherSide && !this.fromStart
-          ? Math.round((horizontal ? height : width) % singleSX) -
+        (this.horizontal ? startY : startX) +
+        (this.fromOtherSide && !this.fromStart
+          ? Math.round((this.horizontal ? height : width) % singleSX) -
             (this.overflow ? singleSX : 0)
           : 0),
-      end = horizontal ? endY : endX,
+      end = this.horizontal ? endY : endX,
       random = this.random ? this.random().one : false,
       stripWidthRandom = this.stripWidthRandom
         ? this.stripWidthRandom.getReal() + 1
@@ -1051,21 +1046,20 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       lengthRandom = this.lengthRandom ? this.lengthRandom.getReal() : 0,
       randomWidth,
       totalWidth,
-      list = args.list,
       l,
-      length = list ? list.length : 0,
+      length = this.args.list ? this.args.list.length : 0,
       draw = this.getDraw(
-        list
+        this.args.list
           ? pixelUnit.push
           : this.getColorArray
             ? this.getColorArray()
             : false,
         /** From Other Side? */
-        horizontal ? fromRight : fromBottom,
+        this.horizontal ? this.fromRight : this.fromBottom,
         stripWidth,
         end,
-        horizontal ? startX : startY,
-        horizontal ? endX : endY,
+        this.horizontal ? startX : startY,
+        this.horizontal ? endX : endY,
         this.overflow,
       )
 
@@ -1081,7 +1075,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
         totalWidth = 1
       }
 
-      if (!cut || start + totalWidth <= end) {
+      if (!this.cut || start + totalWidth <= end) {
         draw(
           start,
           (lengthRandom !== 0 ? Math.round(lengthRandom * random()) : 0) +
@@ -1091,11 +1085,11 @@ export const DrawingTools = function (pixelUnit, getRandom) {
           randomWidth,
         )
 
-        if (list) {
+        if (this.args.list) {
           l = length
 
           while (l--) {
-            list[l].draw()
+            this.args.list[l].draw()
           }
 
           pixelUnit.pop()
@@ -1158,14 +1152,13 @@ export const DrawingTools = function (pixelUnit, getRandom) {
 
   this.Panels.prototype.init = (function (pX) {
     return function (args) {
-      var panels = args.panels,
-        l = panels.length,
+      var l = args.panels.length,
         inherit = {},
         newPanels = (this.args.list = []),
         current
 
       while (l--) {
-        current = panels[l]
+        current = args.panels[l]
 
         if (current.sX) {
           current.sX.autoUpdate = true
@@ -1207,10 +1200,7 @@ export const DrawingTools = function (pixelUnit, getRandom) {
   this.Panels.prototype.draw = (function () {
     // Draws Object, consisting of other Objects and Primitives.
     return function () {
-      var args = this.args,
-        list = args.list,
-        countX,
-        countY
+      var countX, countY
 
       this.dimensions = this.dimensions.calc()
 
@@ -1227,15 +1217,15 @@ export const DrawingTools = function (pixelUnit, getRandom) {
       this.countY = countY
 
       // Find best combination of rows/cols
-      this.findBestRows(list)
+      this.findBestRows(this.args.list)
 
-      var panels = this.sortRows(list)
+      var panels = this.sortRows(this.args.list)
 
       // calculate the finale size of the panel
       this.calcPanelsSizes(panels)
 
       // Draw the content of the panels
-      this.drawPanels(panels, args.mask)
+      this.drawPanels(panels, this.args.mask)
     }
   })(pixelUnit)
 

src/renderengine/helper.js
@@ -148,7 +148,6 @@ export const helper = new (function () {
       changersRelativeCustomList = [],
       changersColorStandardList = [],
       changersCustomList = [],
-      setValue = that.setValue,
       pushRelativeStandard = function (min, max, map, variable) {
         changersRelativeStandardList.push({
           change: max - min,
@@ -158,22 +157,19 @@ export const helper = new (function () {
         })
       },
       changeColor = function (value, map) {
-        var maxColor = map.max,
-          minColor = map.min,
-          maxR = maxColor[0],
-          maxG = maxColor[1],
-          maxB = maxColor[2],
-          minR = minColor[0],
-          minG = minColor[1],
-          minB = minColor[2],
-          color = map.color,
+        var maxR = map.max[0],
+          maxG = map.max[1],
+          maxB = map.max[2],
+          minR = map.min[0],
+          minG = map.min[1],
+          minB = map.min[2],
           valueNeg = 1 - value
 
-        color[0] = minR * valueNeg + maxR * value
+        map.color[0] = minR * valueNeg + maxR * value
 
-        color[1] = minG * valueNeg + maxG * value
+        map.color[1] = minG * valueNeg + maxG * value
 
-        color[2] = minB * valueNeg + maxB * value
+        map.color[2] = minB * valueNeg + maxB * value
       }
 
     return {
@@ -241,7 +237,7 @@ export const helper = new (function () {
               current = changersRelativeStandard[l]
 
               if (args[current.map] !== undefined) {
-                setValue(
+                that.setValue(
                   current.variable,
                   current.min + current.change * args[current.map],
                 )
@@ -255,7 +251,7 @@ export const helper = new (function () {
               current = changersRelativeCustom[l]
 
               if ((currentValue = current[1](args)) !== undefined) {
-                setValue(current[0], currentValue)
+                that.setValue(current[0], currentValue)
               }
             }
           }
@@ -283,7 +279,7 @@ export const helper = new (function () {
       },
 
       ready() {
-        setValue = that.setValueNew
+        that.setValue = that.setValueNew
       },
     }
   }

commit 6f005e4abd3b8c87898df445cc0372641f3255d7

Fix "faceversion" of "Table Comic"
This was accidentally broken in c3dc1ff5753101929e600bb64c26a53de0df3ffc via #28

src/scripts/tableComic2/tablecomic-1.js
@@ -3,7 +3,7 @@ export const getFace = function () {
   const backgroundColor = [0, 0, 0]
   const that = this
   const getPanel = function () {
-    const actor = new this.basic.Actor({
+    const actor = new that.basic.Actor({
       color: [that.rInt(0, 255), that.rInt(0, 255), that.rInt(0, 255)],
     })
     const pupilS = { map: 'c', min: 0.5, max: 0.8 }
@@ -16,10 +16,10 @@ export const getFace = function () {
       },
       list: [
         {
-          what: new this.basic.Stage(),
+          what: new that.basic.Stage(),
         },
         {
-          what: new this.basic.Head({
+          what: new that.basic.Head({
             actor,
             baseSX_: 1,
             noHat: true,
@@ -59,8 +59,8 @@ export const getFace = function () {
   return {
     paperColor: backgroundColor,
     ratio: 1,
-    basicPanel: this.panel,
-    background: new this.basic.Background({
+    basicPanel: that.panel,
+    background: new that.basic.Background({
       blank: true,
       backgroundColor,
     }),
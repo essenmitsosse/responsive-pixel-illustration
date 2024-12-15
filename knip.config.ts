60ecca54306c2af57c4973962a5ec893e333bc87
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-15 22:54:15 +0100

    Add dependency cruiser

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,8 @@ import type { KnipConfig } from 'knip'
 
 const knipConfig: KnipConfig = {
   entry: ['./src/main.ts!', 'eslint.config.ts'],
+  ignore: ['./.dependency-cruiser.js'],
+  ignoreBinaries: ['dot'],
   project: ['**/*.{js,ts}'],
 }
 

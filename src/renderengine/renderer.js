f3eb3360cdef43e6888f61fae9c9a873747b4339
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-30 00:47:01 +0100

    Extract function from prototype

--- src/renderengine/renderer.js
+++ src/renderengine/renderer.js
@@ -231,6 +231,65 @@ const getDrawer = (pixelStarter, renderList) => {
   }
 }
 
+const getRenderPixelToImage =
+  (backgroundColor) => (pixelW, pixelH, pixelArray, imageData) => {
+    let pW = pixelW
+    let w4 = pW * 4
+
+    const wFull = w4
+
+    let pH
+
+    const pHSave = pixelH
+    const fullSave = w4 * pHSave
+
+    let full
+    let c
+    let i
+    let row
+
+    const pA = pixelArray
+    const defaultRed = backgroundColor && backgroundColor[0]
+    const defaultGreen = backgroundColor && backgroundColor[1]
+    const defaultBlue = backgroundColor && backgroundColor[2]
+
+    while (pW--) {
+      w4 -= 4
+
+      pH = pHSave
+
+      full = fullSave
+
+      row = pA[pW]
+
+      while (pH--) {
+        if ((c = row[pH].s.pop())) {
+          c = c.c
+
+          imageData[(i = w4 + (full -= wFull))] = c[0]
+
+          imageData[(i += 1)] = c[1]
+
+          imageData[(i += 1)] = c[2]
+
+          imageData[(i += 1)] = 255
+        } else if (backgroundColor) {
+          imageData[(i = w4 + (full -= wFull))] = defaultRed
+
+          imageData[(i += 1)] = defaultGreen
+
+          imageData[(i += 1)] = defaultBlue
+
+          imageData[(i += 1)] = 255
+        } else {
+          imageData[(i = w4 + (full -= wFull) + 3)] = 0
+        }
+      }
+    }
+
+    return imageData
+  }
+
 export const Renderer = function (canvas, info, options, pixelStarter) {
   // Render Engine to convert basic image into absolute Pixels
   const context = canvas.getContext('2d')
@@ -243,7 +302,7 @@ export const Renderer = function (canvas, info, options, pixelStarter) {
 
   const drawer = getDrawer(pixelStarter, options.imageFunction.renderList)
 
-  const renderPixelToImage = this.getRenderPixelToImage(
+  const renderPixelToImage = getRenderPixelToImage(
     options.imageFunction.background,
   )
 
@@ -315,65 +374,5 @@ export const Renderer = function (canvas, info, options, pixelStarter) {
 
       return [w, h, time]
     },
-  }
-}
-
-Renderer.prototype.getRenderPixelToImage = function (backgroundColor) {
-  return function renderPixelToImage(pixelW, pixelH, pixelArray, imageData) {
-    let pW = pixelW
-    let w4 = pW * 4
-
-    const wFull = w4
-
-    let pH
-
-    const pHSave = pixelH
-    const fullSave = w4 * pHSave
-
-    let full
-    let c
-    let i
-    let row
-
-    const pA = pixelArray
-    const defaultRed = backgroundColor && backgroundColor[0]
-    const defaultGreen = backgroundColor && backgroundColor[1]
-    const defaultBlue = backgroundColor && backgroundColor[2]
-
-    while (pW--) {
-      w4 -= 4
-
-      pH = pHSave
-
-      full = fullSave
-
-      row = pA[pW]
-
-      while (pH--) {
-        if ((c = row[pH].s.pop())) {
-          c = c.c
-
-          imageData[(i = w4 + (full -= wFull))] = c[0]
-
-          imageData[(i += 1)] = c[1]
-
-          imageData[(i += 1)] = c[2]
-
-          imageData[(i += 1)] = 255
-        } else if (backgroundColor) {
-          imageData[(i = w4 + (full -= wFull))] = defaultRed
-
-          imageData[(i += 1)] = defaultGreen
-
-          imageData[(i += 1)] = defaultBlue
-
-          imageData[(i += 1)] = 255
-        } else {
-          imageData[(i = w4 + (full -= wFull) + 3)] = 0
-        }
-      }
-    }
-
-    return imageData
   }
 }

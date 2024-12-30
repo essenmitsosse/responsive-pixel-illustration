5a2e7ee273d96fced99d665a481db8c81609fd28
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-30 19:31:17 +0100

    Convert prototype-based object to `class`

--- src/renderengine/getPixelUnits/index.js
+++ src/renderengine/getPixelUnits/index.js
@@ -563,96 +563,96 @@ const getPixelUnits = () => {
     })
   }
 
-  const Dimensions = function (args, fromRight, fromBottom, rotate) {
-    if (args.sX === undefined) {
-      args.sX = args.s
+  class Dimensions {
+    constructor(args, fromRight, fromBottom, rotate) {
+      if (args.sX === undefined) {
+        args.sX = args.s
+      }
+
+      if (args.sY === undefined) {
+        args.sY = args.s
+      }
+
+      this.x = new AxisX(
+        rotate
+          ? {
+              size: args.sY,
+              pos: args.y,
+              margin: args.mY || args.m,
+              fromOtherSide: fromRight,
+              toOtherSide: args.tY,
+              min: args.minY,
+              center: args.cY || args.c,
+            }
+          : {
+              size: args.sX,
+              pos: args.x,
+              margin: args.mX || args.m,
+              fromOtherSide: fromRight,
+              toOtherSide: args.tX,
+              min: args.minX,
+              center: args.cX || args.c,
+            },
+      )
+
+      this.y = new AxisY(
+        rotate
+          ? {
+              size: args.sX,
+              pos: args.x,
+              margin: args.mX || args.m,
+              fromOtherSide: fromBottom,
+              toOtherSide: args.tX,
+              min: args.minX,
+              center: args.cX || args.c,
+            }
+          : {
+              size: args.sY,
+              pos: args.y,
+              margin: args.mY || args.m,
+              fromOtherSide: fromBottom,
+              toOtherSide: args.tY,
+              min: args.minY,
+              center: args.cY || args.c,
+            },
+      )
     }
 
-    if (args.sY === undefined) {
-      args.sY = args.s
-    }
-
-    this.x = new AxisX(
-      rotate
-        ? {
-            size: args.sY,
-            pos: args.y,
-            margin: args.mY || args.m,
-            fromOtherSide: fromRight,
-            toOtherSide: args.tY,
-            min: args.minY,
-            center: args.cY || args.c,
-          }
-        : {
-            size: args.sX,
-            pos: args.x,
-            margin: args.mX || args.m,
-            fromOtherSide: fromRight,
-            toOtherSide: args.tX,
-            min: args.minX,
-            center: args.cX || args.c,
-          },
-    )
-
-    this.y = new AxisY(
-      rotate
-        ? {
-            size: args.sX,
-            pos: args.x,
-            margin: args.mX || args.m,
-            fromOtherSide: fromBottom,
-            toOtherSide: args.tX,
-            min: args.minX,
-            center: args.cX || args.c,
-          }
-        : {
-            size: args.sY,
-            pos: args.y,
-            margin: args.mY || args.m,
-            fromOtherSide: fromBottom,
-            toOtherSide: args.tY,
-            min: args.minY,
-            center: args.cY || args.c,
-          },
-    )
-  }
-
-  Dimensions.prototype = {
     get width() {
       return this.x.realSize
-    },
+    }
     get height() {
       return this.y.realSize
-    },
+    }
     get posX() {
       return this.x.realPos
-    },
+    }
     get posY() {
       return this.y.realPos
-    },
+    }
     get endX() {
       return this.x.realPos + this.x.realSize
-    },
+    }
     get endY() {
       return this.y.realPos + this.y.realSize
-    },
-  }
+    }
 
-  Dimensions.prototype.calc = function () {
-    this.x.calc()
+    calc() {
+      this.x.calc()
 
-    this.y.calc()
+      this.y.calc()
 
-    return this
-  }
+      return this
+    }
 
-  Dimensions.prototype.checkMin = function () {
-    return (
-      this.x.realSize < 1 ||
-      (this.x.min && this.x.realSize < this.x.min.getReal()) ||
-      this.y.realSize < 1 ||
-      (this.y.min && this.y.realSize < this.y.min.getReal())
-    )
+    checkMin() {
+      return (
+        this.x.realSize < 1 ||
+        (this.x.min && this.x.realSize < this.x.min.getReal()) ||
+        this.y.realSize < 1 ||
+        (this.y.min && this.y.realSize < this.y.min.getReal())
+      )
+    }
   }
 
   return {

eacd3f88abad09fbfcaf7024ba9cf4e9d56b3bce
Marcus Blättermann <marcus@essenmitsosse.de>
2024-12-30 17:50:34 +0100

    Remove unnecessary IIFE
    Similar to the two previous commits

--- src/renderengine/getPixelUnits/index.js
+++ src/renderengine/getPixelUnits/index.js
@@ -518,150 +518,143 @@ const getPixelUnits = () => {
     AxisY.prototype.dim = PosY.prototype.dim = dimensions.height
   }
 
-  const twoD = (() => {
-    const Position = function (args, reflectX, reflectY, rotate) {
-      const fromRight = (args.fX || false) !== reflectX
-      const fromBottom = (args.fY || false) !== reflectY
+  const Position = function (args, reflectX, reflectY, rotate) {
+    const fromRight = (args.fX || false) !== reflectX
+    const fromBottom = (args.fY || false) !== reflectY
 
-      const x = new PosX(
-        rotate
-          ? {
-              pos: args.y,
-              fromOtherSide: !fromBottom,
-              toOtherSide: args.toTop,
-              center: args.centerX || args.center,
-            }
-          : {
-              pos: args.x,
-              fromOtherSide: fromRight,
-              toOtherSide: args.toLeft,
-              center: args.centerY || args.center,
-            },
-      )
+    const x = new PosX(
+      rotate
+        ? {
+            pos: args.y,
+            fromOtherSide: !fromBottom,
+            toOtherSide: args.toTop,
+            center: args.centerX || args.center,
+          }
+        : {
+            pos: args.x,
+            fromOtherSide: fromRight,
+            toOtherSide: args.toLeft,
+            center: args.centerY || args.center,
+          },
+    )
 
-      const y = new PosY(
-        rotate
-          ? {
-              pos: args.x,
-              fromOtherSide: fromRight,
-              toOtherSide: args.toLeft,
-              center: args.centerX || args.center,
-            }
-          : {
-              pos: args.y,
-              fromOtherSide: fromBottom,
-              toOtherSide: args.toTop,
-              center: args.centerY || args.center,
-            },
-      )
+    const y = new PosY(
+      rotate
+        ? {
+            pos: args.x,
+            fromOtherSide: fromRight,
+            toOtherSide: args.toLeft,
+            center: args.centerX || args.center,
+          }
+        : {
+            pos: args.y,
+            fromOtherSide: fromBottom,
+            toOtherSide: args.toTop,
+            center: args.centerY || args.center,
+          },
+    )
 
-      return () => ({
-        x: x.calc(),
-        y: y.calc(),
-      })
+    return () => ({
+      x: x.calc(),
+      y: y.calc(),
+    })
+  }
+
+  const Dimensions = function (args, fromRight, fromBottom, rotate) {
+    if (args.sX === undefined) {
+      args.sX = args.s
     }
 
-    const Dimensions = function (args, fromRight, fromBottom, rotate) {
-      if (args.sX === undefined) {
-        args.sX = args.s
-      }
-
-      if (args.sY === undefined) {
-        args.sY = args.s
-      }
-
-      this.x = new AxisX(
-        rotate
-          ? {
-              size: args.sY,
-              pos: args.y,
-              margin: args.mY || args.m,
-              fromOtherSide: fromRight,
-              toOtherSide: args.tY,
-              min: args.minY,
-              center: args.cY || args.c,
-            }
-          : {
-              size: args.sX,
-              pos: args.x,
-              margin: args.mX || args.m,
-              fromOtherSide: fromRight,
-              toOtherSide: args.tX,
-              min: args.minX,
-              center: args.cX || args.c,
-            },
-      )
-
-      this.y = new AxisY(
-        rotate
-          ? {
-              size: args.sX,
-              pos: args.x,
-              margin: args.mX || args.m,
-              fromOtherSide: fromBottom,
-              toOtherSide: args.tX,
-              min: args.minX,
-              center: args.cX || args.c,
-            }
-          : {
-              size: args.sY,
-              pos: args.y,
-              margin: args.mY || args.m,
-              fromOtherSide: fromBottom,
-              toOtherSide: args.tY,
-              min: args.minY,
-              center: args.cY || args.c,
-            },
-      )
+    if (args.sY === undefined) {
+      args.sY = args.s
     }
 
-    Dimensions.prototype = {
-      get width() {
-        return this.x.realSize
-      },
-      get height() {
-        return this.y.realSize
-      },
-      get posX() {
-        return this.x.realPos
-      },
-      get posY() {
-        return this.y.realPos
-      },
-      get endX() {
-        return this.x.realPos + this.x.realSize
-      },
-      get endY() {
-        return this.y.realPos + this.y.realSize
-      },
-    }
+    this.x = new AxisX(
+      rotate
+        ? {
+            size: args.sY,
+            pos: args.y,
+            margin: args.mY || args.m,
+            fromOtherSide: fromRight,
+            toOtherSide: args.tY,
+            min: args.minY,
+            center: args.cY || args.c,
+          }
+        : {
+            size: args.sX,
+            pos: args.x,
+            margin: args.mX || args.m,
+            fromOtherSide: fromRight,
+            toOtherSide: args.tX,
+            min: args.minX,
+            center: args.cX || args.c,
+          },
+    )
 
-    Dimensions.prototype.calc = function () {
-      this.x.calc()
+    this.y = new AxisY(
+      rotate
+        ? {
+            size: args.sX,
+            pos: args.x,
+            margin: args.mX || args.m,
+            fromOtherSide: fromBottom,
+            toOtherSide: args.tX,
+            min: args.minX,
+            center: args.cX || args.c,
+          }
+        : {
+            size: args.sY,
+            pos: args.y,
+            margin: args.mY || args.m,
+            fromOtherSide: fromBottom,
+            toOtherSide: args.tY,
+            min: args.minY,
+            center: args.cY || args.c,
+          },
+    )
+  }
 
-      this.y.calc()
+  Dimensions.prototype = {
+    get width() {
+      return this.x.realSize
+    },
+    get height() {
+      return this.y.realSize
+    },
+    get posX() {
+      return this.x.realPos
+    },
+    get posY() {
+      return this.y.realPos
+    },
+    get endX() {
+      return this.x.realPos + this.x.realSize
+    },
+    get endY() {
+      return this.y.realPos + this.y.realSize
+    },
+  }
 
-      return this
-    }
+  Dimensions.prototype.calc = function () {
+    this.x.calc()
 
-    Dimensions.prototype.checkMin = function () {
-      return (
-        this.x.realSize < 1 ||
-        (this.x.min && this.x.realSize < this.x.min.getReal()) ||
-        this.y.realSize < 1 ||
-        (this.y.min && this.y.realSize < this.y.min.getReal())
-      )
-    }
+    this.y.calc()
 
-    return {
-      Position,
-      Dimensions,
-    }
-  })()
+    return this
+  }
+
+  Dimensions.prototype.checkMin = function () {
+    return (
+      this.x.realSize < 1 ||
+      (this.x.min && this.x.realSize < this.x.min.getReal()) ||
+      this.y.realSize < 1 ||
+      (this.y.min && this.y.realSize < this.y.min.getReal())
+    )
+  }
 
   return {
-    Position: twoD.Position,
-    Dimensions: twoD.Dimensions,
+    Position,
+    Dimensions,
     createSize,
     Width,
     Height,

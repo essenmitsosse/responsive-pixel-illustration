c6520e999899fc9b2e43bb6feb9cc61dd7b1fcb4
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-02 01:13:19 +0100

    Extract function

--- src/renderengine/creator.js
+++ src/renderengine/creator.js
@@ -23,32 +23,34 @@ const getRandom = (() => {
   }
 })()
 
+const getSeedHandler = () => {
+  const getSeed = getRandom().seed
+
+  let count = 0
+
+  const i = []
+
+  return {
+    reset() {
+      let l = count
+
+      while (l--) {
+        i[l] = 0
+      }
+    },
+    get(j) {
+      const seed = j || getSeed()
+      const nr = (count += 1)
+
+      return function () {
+        return getRandom(seed + i[nr]++ || 0)
+      }
+    },
+  }
+}
+
 export const DrawingTools = function (pixelUnit) {
-  const seed = (function (getRandom) {
-    const getSeed = getRandom().seed
-
-    let count = 0
-
-    const i = []
-
-    return {
-      reset() {
-        let l = count
-
-        while (l--) {
-          i[l] = 0
-        }
-      },
-      get(j) {
-        const seed = j || getSeed()
-        const nr = (count += 1)
-
-        return function () {
-          return getRandom(seed + i[nr]++ || 0)
-        }
-      },
-    }
-  })(getRandom)
+  const seed = getSeedHandler()
 
   const pixelSetter = (function () {
     let colorArray

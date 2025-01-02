4c6d2de2d4dd42f6778ed6ec172be1360b957ff8
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-02 02:50:26 +0100

    Extract class to own file

---
+++ src/renderengine/creator/RoundRect.js
@@ -0,0 +1,34 @@
+import Obj from './Obj'
+
+class RoundRect extends Obj {
+  getName = 'Rounded Rectangle'
+
+  list = [
+    // { mY:1 },
+    // { mX:1, height: {a:1} },
+    // { mX:1, height: {a:1}, fromBottom:true },
+    {
+      minX: 3,
+      minY: 4,
+      list: [
+        { name: 'Dot', clear: true },
+        { name: 'Dot', fX: true, clear: true },
+        { name: 'Dot', fY: true, clear: true },
+        { name: 'Dot', fX: true, fY: true, clear: true },
+      ],
+    },
+    {
+      minX: 4,
+      minY: 3,
+      list: [
+        { name: 'Dot', clear: true },
+        { name: 'Dot', fX: true, clear: true },
+        { name: 'Dot', fY: true, clear: true },
+        { name: 'Dot', fX: true, fY: true, clear: true },
+      ],
+    },
+    {},
+  ]
+}
+
+export default RoundRect
--- src/renderengine/creator/index.js
+++ src/renderengine/creator/index.js
@@ -9,38 +9,8 @@ import Obj from './Obj'
 import Polygon from './Polygon'
 import Primitive from './Primitive'
 import Rect from './Rect'
+import RoundRect from './RoundRect'
 import Stripes from './Stripes'
-
-class RoundRect extends Obj {
-  getName = 'Rounded Rectangle'
-
-  list = [
-    // { mY:1 },
-    // { mX:1, height: {a:1} },
-    // { mX:1, height: {a:1}, fromBottom:true },
-    {
-      minX: 3,
-      minY: 4,
-      list: [
-        { name: 'Dot', clear: true },
-        { name: 'Dot', fX: true, clear: true },
-        { name: 'Dot', fY: true, clear: true },
-        { name: 'Dot', fX: true, fY: true, clear: true },
-      ],
-    },
-    {
-      minX: 4,
-      minY: 3,
-      list: [
-        { name: 'Dot', clear: true },
-        { name: 'Dot', fX: true, clear: true },
-        { name: 'Dot', fY: true, clear: true },
-        { name: 'Dot', fX: true, fY: true, clear: true },
-      ],
-    },
-    {},
-  ]
-}
 
 class Grid extends Obj {
   getName = 'Grid'

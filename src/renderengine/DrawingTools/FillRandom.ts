c56a93391c99b4b07b628b2452cefbf00331f2eb
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-04 23:54:57 +0100

    Revert "Add type check"
    This reverts commit b065265fbe6c968ff42eae0325a16df6b757087c.
    
    This was unnecessary and actually caused a bug.

--- src/renderengine/DrawingTools/FillRandom.ts
+++ src/renderengine/DrawingTools/FillRandom.ts
@@ -99,11 +99,6 @@ class FillRandom extends Fill {
       : false
 
     const array = this.state.pixelSetter.getSave(this.use)
-
-    if (array === false) {
-      throw new Error('unexpected Error: save array was not found')
-    }
-
     const l = array ? array.length : 0
 
     let count = Math.floor(

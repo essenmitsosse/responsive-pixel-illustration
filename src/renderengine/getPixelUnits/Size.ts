d9e9143d734de70bc48803dec05122f9ab3a9c23
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-07 17:54:44 +0100

    Convert `while` to `.map.reduce`

--- src/renderengine/getPixelUnits/Size.ts
+++ src/renderengine/getPixelUnits/Size.ts
@@ -1,3 +1,5 @@
+import getSumForReduce from '@/lib/getSumForReduce'
+
 import type { State } from './State'
 import type { InputDynamicVariable } from '@/helper/typeSize'
 import type { DynamicVariable } from '@/renderengine/Variable'
@@ -311,14 +313,10 @@ export class Dimension {
       throw new Error('Unexpected error: adder is undefined')
     }
 
-    let add = 0
-    let l = this.adder.length
-
-    while (l--) {
-      add += this.adder[l].getReal()
-    }
-
-    return this.getRealDistanceBasic() + add
+    return (
+      this.getRealDistanceBasic() +
+      this.adder.map((size) => size.getReal()).reduce(getSumForReduce, 0)
+    )
   }
 
   getRealDistanceWithCalcOnlyAdding(): number {

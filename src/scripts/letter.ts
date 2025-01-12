3a3871e01aa8e985ce54a14065a805655cf0b34a
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-12 19:48:35 +0100

    Add types

--- src/scripts/letter.ts
+++ src/scripts/letter.ts
@@ -1,11 +1,15 @@
 import getHoverChangerStandard from '@/helper/getHoverChangerStandard'
 
-const letter = (args, init, createSlider) => {
+import type { ImageFunction, InputDynamicLink } from './listImage'
+import type { InputDynamicVariableBase } from '@/helper/typeSize'
+import type { Tool } from '@/renderengine/DrawingTools/Primitive'
+
+const letter: ImageFunction = (_, init, createSlider) => {
   const hoverChangerStandard = getHoverChangerStandard()
   const backgroundColor = [200, 200, 240]
-  const linkList = []
+  const linkList: Array<InputDynamicLink> = []
 
-  const linkListPush = (obj) => {
+  const linkListPush = function <T extends InputDynamicLink>(obj: T): T {
     linkList.push(obj)
 
     return obj
@@ -66,7 +70,11 @@ const letter = (args, init, createSlider) => {
     min: 1,
   })
 
-  const getLetter = function (args) {
+  const getLetter = function (args: {
+    letter: ReadonlyArray<Tool>
+    x?: InputDynamicVariableBase
+    y?: InputDynamicVariableBase
+  }): Tool {
     return {
       x: args.x,
       y: args.y,
@@ -109,7 +117,7 @@ const letter = (args, init, createSlider) => {
     }
   }
 
-  const renderList = [
+  const renderList: ReadonlyArray<Tool | undefined> = [
     getLetter({
       letter: [
         // Stämme

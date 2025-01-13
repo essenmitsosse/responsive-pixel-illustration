b41d43f710efaa581033a42e3223d8ddaf844ac4
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-14 00:23:12 +0100

    Convert prototype-based object to `class`

--- src/scripts/turnAround/object.js
+++ src/scripts/turnAround/object.js
@@ -240,48 +240,50 @@ export class Rotater extends BBObj {
   }
 }
 
-export const RotateInfo = function (rotate) {
-  const s = { a: 5 }
+export class RotateInfo extends BBObj {
+  constructor(rotate) {
+    super()
 
-  this.ll.push(s)
+    const s = { a: 5 }
 
-  return {
-    color: this.black,
-    s: [s, s, 1],
-    x: { r: 0.02 },
-    y: { r: 0.02 },
-    rX: true,
-    list: [
-      { sX: 1, cX: true },
-      { sY: 1, cY: true },
-      {
-        color: [150, 150, 150],
-        c: true,
-        s,
-        list: [
-          {},
-          { sX: 1, color: this.c1 },
-          { sY: 1, fY: true, color: this.c1D },
-        ],
-      },
-      {
-        s: 1,
-        c: true,
-        list: [
-          {
-            color: this.c2D,
-            points: [
-              {},
-              {
-                x: { useSize: s, r: rotate.sin },
-                y: { useSize: s, r: rotate.cos },
-              },
-            ],
-          },
-        ],
-      },
-    ],
+    this.ll.push(s)
+
+    return {
+      color: this.black,
+      s: [s, s, 1],
+      x: { r: 0.02 },
+      y: { r: 0.02 },
+      rX: true,
+      list: [
+        { sX: 1, cX: true },
+        { sY: 1, cY: true },
+        {
+          color: [150, 150, 150],
+          c: true,
+          s,
+          list: [
+            {},
+            { sX: 1, color: this.c1 },
+            { sY: 1, fY: true, color: this.c1D },
+          ],
+        },
+        {
+          s: 1,
+          c: true,
+          list: [
+            {
+              color: this.c2D,
+              points: [
+                {},
+                {
+                  x: { useSize: s, r: rotate.sin },
+                  y: { useSize: s, r: rotate.cos },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }
   }
 }
-
-RotateInfo.prototype = new BBObj()

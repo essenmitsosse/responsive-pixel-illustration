7b7d29a12772ea8b71a9c07d488c3bd8addd31a5
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-16 02:09:04 +0100

    Remove unnecessary constructor

--- src/scripts/turnAround/LowerBody.js
+++ src/scripts/turnAround/LowerBody.js
@@ -2,14 +2,6 @@ import BBObj from './BBObj'
 
 // LOWER BODY  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class LowerBody extends BBObj {
-  constructor(args) {
-    super()
-
-    this.color = args.color
-
-    this.colorDark = args.colorDark
-  }
-
   draw(args, front, right) {
     return [
       {

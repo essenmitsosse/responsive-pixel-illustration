0862d7b60534a5b04cfe6d249b147f1c7162f21b
Marcus Blättermann <marcus@essenmitsosse.de>
2025-01-26 23:51:58 +0100

    Extract method to function

--- src/scripts/turnAround/BBObj.ts
+++ src/scripts/turnAround/BBObj.ts
@@ -51,6 +51,24 @@ export type Rotation = {
   sin: number
   turnedAway: number
 }
+
+const merge = <T extends Pick<What, 'x' | 'y' | 'z'>>(
+  what: T,
+  args: {
+    x: InputDynamicVariable
+    y: InputDynamicVariable
+    z: number
+  },
+): T => {
+  what.x = args.x
+
+  what.y = args.y
+
+  what.z = args.z
+
+  return what
+}
+
 class BBObj {
   declare ll: Array<InputDynamicVariable>
 
@@ -177,7 +195,7 @@ class BBObj {
 
     what.x = x = this.moveOut(move, what.rotate)
 
-    what.get = this.merge(what.get, {
+    what.get = merge(what.get, {
       x,
       y: move.y,
       z:
@@ -191,23 +209,6 @@ class BBObj {
         (move.z || 50) *
         what.rotate.turnedAway,
     })
-
-    return what
-  }
-
-  merge<T extends Pick<What, 'x' | 'y' | 'z'>>(
-    what: T,
-    args: {
-      x: InputDynamicVariable
-      y: InputDynamicVariable
-      z: number
-    },
-  ): T {
-    what.x = args.x
-
-    what.y = args.y
-
-    what.z = args.z
 
     return what
   }
